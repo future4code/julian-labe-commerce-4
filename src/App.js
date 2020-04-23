1351660c133ddb62a35d34345c93e0cafa03b68d
Géssica Costa de Araújo Souza <gcas_@hotmail.com>
2020-04-23 12:06:21 -0300

    Fiz a estrutura da Home.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Produtos from './components/Produtos'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    produtos: [
+      {
+        id: 1,
+        name: "Foguete da Missão Apollo 11",
+        value: 10000.0,
+        imageUrl: "https://picsum.photos/400/400?a=1",
+      },
+      {
+        id: 1,
+        name: "Foguete da Missão Apollo 11",
+        value: 10000.0,
+        imageUrl: "https://picsum.photos/400/400?a=2",
+      },
+      {
+        id: 1,
+        name: "Foguete da Missão Apollo 11",
+        value: 10000.0,
+        imageUrl: "https://picsum.photos/400/400?a=3",
+      },
+      {
+        id: 1,
+        name: "Foguete da Missão Apollo 11",
+        value: 10000.0,
+        imageUrl: "https://picsum.photos/400/400?a=4",
+      },
+      {
+        id: 1,
+        name: "Foguete da Missão Apollo 11",
+        value: 10000.0,
+        imageUrl: "https://picsum.photos/400/400?a=5",
+      },
+      {
+        id: 1,
+        name: "Foguete da Missão Apollo 11",
+        value: 10000.0,
+        imageUrl: "https://picsum.photos/400/400?a=6",
+      },
+      {
+        id: 1,
+        name: "Foguete da Missão Apollo 11",
+        value: 10000.0,
+        imageUrl: "https://picsum.photos/400/400?a=7",
+      },
+      {
+        id: 1,
+        name: "Foguete da Missão Apollo 11",
+        value: 10000.0,
+        imageUrl: "https://picsum.photos/400/400?a=8",
+      }
+    ]
+  }
+  render() {
+    const listaDeProdutos = this.state.produtos.map((produto, index) => {
+      return (
+        <Produtos key={index}
+          name={produto.name}
+          value={produto.value}
+          image={produto.imageUrl}
+        />
+      )
+    })
+    return (
+      <div className="App">
+        <div>
+          <p>Quantidade de Produtos: </p>
+          <select>
+            <option value='crescente'>Preço: Crescente</option>
+            <option value='decrescente'>Preço: Decrescente</option>
+          </select>
+          {listaDeProdutos}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
