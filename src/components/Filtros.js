0071d61cf8930a5d6e4c8ab263edd44ea83f5b4a
Géssica Costa de Araújo Souza <gcas_@hotmail.com>
2020-04-26 23:30:18 -0300

    Total do Carrinho soma produtos adicionados e botão de excluir produto criado.

--- src/components/Filtros.js
+++ src/components/Filtros.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
 import Produtos from './Produtos'
+import Carrinho from './Carrinho'
 
 const Container = styled.div`
 display: grid;
-grid-template-columns: 1fr 5fr;
+grid-template-columns: 1fr 5fr 1fr;
 gap: 10px;
 `
 const FiltrosContainer = styled.div`
@@ -18,12 +19,18 @@ grid-template-columns: repeat(auto-fill, minmax(180px, 1fr));
 grid-column: 2/3;
 gap: 1px;
 `
+const CarrinhoContainer = styled.div`
+grid-column: 3/4;
+border: 1px solid white;
+`
 
 class Filtros extends React.Component {
     state = {
         minimo: '',
         maximo: '',
-        buscaProduto: ''
+        buscaProduto: '',
+        compras: [],
+        total: 0
     }
 
     onChangeMinimo = (event) => {
@@ -38,10 +45,47 @@ class Filtros extends React.Component {
         this.setState({ buscaProduto: event.target.value })
     }
 
+    onClickComprar = (produto) => {
+        console.log('clicou', produto)
+        this.state.compras.push(produto)
+        let somatorio = 0
+        const soma = this.state.compras.map(produto => {
+            somatorio += produto.value
+        })
+        this.setState({ total: somatorio })
+    }
+
+    componentDidUpdate() {
+        console.log('update')
+    }
+
+    onClickApagar = (id) => {
+        let somatorio = 0
+
+        const novoCarrinho = this.state.compras.filter(produto => {
+            if (id !== produto.id) {
+                return produto
+            }
+        })
+
+        this.setState({ compras: novoCarrinho })
+        
+        if (this.state.compras.length === 0) {
+            somatorio = 0
+            this.setState({ total: somatorio })
+        } else {
+            const soma = this.state.compras.map(produto => {
+                somatorio += produto.value
+            })
+        }
+    }
+
     render() {
         const produtos = this.props.state
         const inputMinusculo = this.state.buscaProduto.toLowerCase()
 
+        console.log('somatorio', this.state.total,'compras tamanho',this.state.compras.length)
+        
         const buscaNome = produtos.filter(produto => {
             let produtoNome = produto.name.toLowerCase()
             return produtoNome.search(inputMinusculo) !== -1
@@ -54,8 +98,6 @@ class Filtros extends React.Component {
                 return produto
             }
         })
-
-        console.log('filtro', filtro)
 
         return (
             <Container>
@@ -73,14 +115,33 @@ class Filtros extends React.Component {
                         filtro.map((produto, index) => {
                             return (
                                 <Produtos key={index}
+                                    id={produto.id}
                                     name={produto.name}
                                     value={produto.value}
                                     image={produto.imageUrl}
+                                    onClickComprar={() => this.onClickComprar(produto)}
                                 />
                             )
                         })
                     }
                 </ListaDeProdutos>
+                <CarrinhoContainer>
+                    <h3>Carrinho</h3>
+                    <p>Total: ${this.state.total}</p>
+                    {
+                        this.state.compras.map((produto, index) => {
+                            return (
+                                <Carrinho key={index}
+                                    id={produto.id}
+                                    name={produto.name}
+                                    value={produto.value}
+                                    image={produto.imageUrl}
+                                    onClickApagar={() => this.onClickApagar(produto.id)}
+                                />
+                            )
+                        })
+                    }
+                </CarrinhoContainer>
             </Container>
         )
     }
